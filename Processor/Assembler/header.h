ad6bbe44858aa6f4bbb617fa6b7712e417e535a2
shugaley <54910986+shugaley@users.noreply.github.com>
2019-10-11 17:44:56 +0300

    Create header.h

---
+++ Processor/Assembler/header.h
@@ -0,0 +1,15 @@
+#ifndef SOFT_PROCCESOR_HEADER_H
+#define SOFT_PROCCESOR_HEADER_H
+
+#endif //SOFT_PROCCESOR_HEADER_H
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <assert.h>
+#include <sys/stat.h>
+
+const char* TEXT_INPUT = "Home/C/ded/Soft-processor/input_file.txt";
+const char* TEXT_OUTPUT = "Home/C/ded/Soft-processor/output_file.txt";

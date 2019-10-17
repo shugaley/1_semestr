409bb7f60f79ba914ed038ea2cb527831a23fec3
shugaley <54910986+shugaley@users.noreply.github.com>
2019-10-17 17:37:28 +0300

    Update Commands.h

--- Processor/Commands.h
+++ Processor/Commands.h
@@ -1,10 +1,15 @@
-CMD(PUSH, 65);
-CMD(POP,  66);
-CMD(ADD,  67);
-CMD(SUB,  68);
-CMD(MUL,  69);
-CMD(DIV,  70);
-CMD(END,  90);
+#ifdef NON_REG_MOD
+
+CMD(PUSH, 65)
+CMD(POP,  66)
+CMD(ADD,  67)
+CMD(SUB,  68)
+CMD(MUL,  69)
+CMD(DIV,  70)
+CMD(END,  90)
+
+#undef NON_REG_MOD
+#endif
 
 
 
@@ -13,12 +18,10 @@ CMD(END,  90);
 
 
 
+#ifdef REG_MOD
 
+CMDR(PUSH, 97)
+CMDR(POP, 98)
 
-
-
-
-
-
-CMDR(PUSH, 97);
-CMDR(POP, 98);
+#undef REG_MOD
+#endif

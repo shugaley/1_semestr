aeb4b3ba6226df3582883a26c82b55f191d522c2
shugaley <54910986+shugaley@users.noreply.github.com>
2019-10-21 22:55:42 +0300

    Update assembler.cpp

--- Processor/Assembler/assembler.cpp
+++ Processor/Assembler/assembler.cpp
@@ -1,4 +1,4 @@
-#include "header_asm.h"
+#include "header_ass.h"
 
 enum FindParams
 {
@@ -6,6 +6,7 @@ enum FindParams
     NUMBER,
     REGISTER,
     RAM,
+
 };
 
 const int MAX_COMMAND_SIZE = 50;
@@ -13,6 +14,10 @@ const int MAX_COMMAND_SIZE = 50;
 
 int assembler(struct LINE* lines_text, char* bytecode) {
     assert(bytecode);
+
+    ST_MARK* marks = (ST_MARK*)calloc(sizeof(lines_text), sizeof(marks[0]));
+    marks->name = (char*)calloc(MAX_COMMAND_SIZE, sizeof(marks->name[0]));
+    marks = CreateMarks(lines_text);
 
     int pc = 0;
     char *str_command = (char*) calloc(MAX_COMMAND_SIZE, sizeof(str_command[0]));
@@ -27,7 +32,7 @@ int assembler(struct LINE* lines_text, char* bytecode) {
 
         int value = 0;
         char* regis = (char*) (calloc)(MAX_COMMAND_SIZE, sizeof(regis[0]));
-        switch (FindParams(str))
+        switch (FindParams(str, &value, marks))
         {
             case NO_PARAMS:
                 bytecode[pc++] = WhichCommand(str_command);
@@ -37,7 +42,6 @@ int assembler(struct LINE* lines_text, char* bytecode) {
 
             case NUMBER:
                 bytecode[pc++] = WhichCommand(str_command);
-                sscanf(str, "%*s %d", &value)
                 *(int*)(bytecode + pc) = value;
                 pc += sizeof(value);
                 break;
@@ -62,8 +66,8 @@ char WhichCommand(char* str_command)
     assert(str_command);
 
     #define DEF_CMD(name, num, code)\
-           if(strcasecmp(str_command, #name) == 0)\
-                return num;
+        if(strcasecmp(str_command, #name) == 0)\
+            return num;
 
     #define NON_REG_MOD
 
@@ -80,14 +84,15 @@ char WhichCommandReg(char* str_command)
 {
     assert(str_command);
 
-#define DEF_CMDR(name, num, code) \
+    #define DEF_CMDR(name, num, code) \
         if(strcasecmp(str_command, #name) == 0)\
             return num;
 
-#define REG_MOD
-#include "commands.h"
+    #define REG_MOD
 
-#undef DEF_CMDR
+    #include "commands.h"
+
+    #undef DEF_CMDR
 
     return 0;
 }
@@ -109,23 +114,64 @@ bool DeleteComm(char* str)
 }
 
 
-int FindParams(char* str)
+int FindParams(char* str, int* value, ST_MARK* marks)
 {
     assert(str);
 
-    int value = 0;
-    if(sscanf(str, "%*s %d", &value) == 1)
+    if(sscanf(str, "%*s %d", value) == 1)
         return NUMBER;
 
     char* param = (char*)calloc(MAX_COMMAND_SIZE, sizeof(param[0]));
     sscanf(str, "%*s %s", param);
 
     if(isalpha(*param))
-        return REGISTER;
+    {
+        if(FindMark(param, value, marks))
+            return NUMBER;
+        else
+            return REGISTER;
+    }
+
 
     free(param);
 
     assert(str);
 
     return NO_PARAMS;
+}
+
+
+
+ST_MARK* CreateMarks(LINE* lines_text)
+{
+    assert(lines_text);
+
+    ST_MARK* marks = (ST_MARK*)calloc(sizeof(lines_text), sizeof(marks[0]));
+    marks->name = (char*)calloc(MAX_COMMAND_SIZE, sizeof(marks->name[0]));
+    for(size_t i_line = 0; i_line < sizeof(lines_text); i_line++)
+    {
+
+        char* str = lines_text[i_line].ptrline;
+        if(strchr(str, ':'))
+        {
+            sscanf(str, "%s", marks[i_line].name);
+            marks[i_line].address = i_line;
+        }
+    }
+}
+
+
+
+bool FindMark(char* param, int* value, ST_MARK* marks)
+{
+    assert(param);
+
+    for(size_t i_line = 0; i_line < sizeof(marks); i_line++)
+        if(!strcasecmp(param, marks[i_line].name))
+        {
+            *value = marks[i_line].address;
+            return true;
+        }
+
+    return false;
 }

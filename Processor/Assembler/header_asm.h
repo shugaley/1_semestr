5271e8f6e3c522a72861a882a71f5e479c8fc02d
shugaley <54910986+shugaley@users.noreply.github.com>
2019-10-21 18:20:47 +0300

    Update and rename header.h to header_asm.h

--- Processor/Assembler/header_asm.h
+++ Processor/Assembler/header_asm.h
@@ -1,27 +1,31 @@
 #include <cstdio>
 #include <cstdlib>
-#include <string>
+#include <cstring>
 #include <cctype>
 #include <cassert>
 #include <sys/stat.h>
 
 
-int assembler(struct LINE* lines_text, size_t nlines, char* bytecode);
+int assembler(struct LINE* lines_text, char* bytecode);
 
 char WhichCommand(char* str_command);
 char WhichCommandReg(char* str_command);
 
-bool DeleteComm(struct LINE* lines_text, int nlines);
+bool DeleteComm(char* str);
 
-int FindParams(struct LINE lines_text, char* param);
+int FindParams(char* str);
 
-struct LINE* create_all(size_t* nlines);
+//Reader---------------
+
+struct LINE* create_all(FILE* ptr_input);
 
 char* create_ar_text(FILE* pnt_input, size_t* size);
 struct LINE* create_struct_lines(char* text, size_t nlines, size_t size);
 
 size_t count_size();
-size_t count_symbol(char* text, size_t size, char symbol);
+size_t count_symbol(const char* text, size_t size, char symbol);
+
+//--------------------------
 
 struct LINE
 {

3445273ebda60d7c7b749e139a562cc557a3ff2e
shugaley <54910986+shugaley@users.noreply.github.com>
2019-11-18 20:16:46 +0300

    Create diff_reader.cpp

---
+++ differentiator/diff_reader.cpp
@@ -0,0 +1,28 @@
+#include "diff_header.h"
+
+char* create_ar_text(FILE* ptr_input)
+{
+
+    assert(ptr_input);
+
+    size_t size = count_size();
+    char* text = (char*)calloc(size + 1, sizeof(text[0]));
+    size = fread(text, sizeof(text[0]), size, ptr_input) + 1;
+    text = (char*)realloc(text, size + 1);
+
+    return text;
+}
+
+
+size_t count_size()
+{
+    struct stat struct_text = {};
+    if (stat(FILE_INP_INORDER, &struct_text))
+    {
+        perror("Failure structure of file creation");
+        abort();
+    }
+
+    size_t size = struct_text.st_size;
+
+   
